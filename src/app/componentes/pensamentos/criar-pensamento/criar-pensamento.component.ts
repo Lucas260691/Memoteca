b9a643fe5aae05830bbd0326278bb64b9aabab22
Lucas <lucasmeinen@yahoo.com.br>
2023-01-09 16:28:31 -0300

    Criando as ações dos botões através do conceito event binding

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -5,11 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './criar-pensamento.component.html',
   styleUrls: ['./criar-pensamento.component.css']
 })
+
 export class CriarPensamentoComponent {
   pensamento = {
     id: '1',
     conteudo: 'Aprendendo Angular',
     autoria: 'Dev',
     modelo: ''
+  }
+
+  criarPensamento(){
+    alert("Novo pensamento criado")
+  }
+
+  cancelar(){
+    alert("Ação cancelada")
   }
 }

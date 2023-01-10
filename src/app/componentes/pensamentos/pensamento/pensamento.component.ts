7a7a5edb3e1ede66813bd6d436e9e6092bc4336d
Lucas <lucasmeinen@yahoo.com.br>
2023-01-10 01:11:47 -0300

    Criando o componente pensamento

---
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.css']
+})
+export class PensamentoComponent {
+
+}

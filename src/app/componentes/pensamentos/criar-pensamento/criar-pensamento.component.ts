da7c726c0ab42f34c126fd0a3855b24f53ce559f
Lucas <lucasmeinen@yahoo.com.br>
2023-01-13 21:48:43 -0300

    Implementando os botoes criarPensamento, cancelar e retefinindo rotas

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,5 +1,7 @@
+import { PensamentoService } from './../pensamento.service';
 import { Pensamento } from './../pensamento/pensamento';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -9,17 +11,27 @@ import { Component } from '@angular/core';
 
 export class CriarPensamentoComponent {
   pensamento: Pensamento = {
-    id: 1,
-    conteudo: 'Aprendendo Angular',
-    autoria: 'Dev',
+    conteudo: '',
+    autoria: '',
     modelo: 'modelo1'
   }
 
+  constructor(
+    private service: PensamentoService,
+    private router: Router
+    ) { }
+
+  ngOnInit(): void {
+
+  }
+
   criarPensamento(){
-    alert("Novo pensamento criado")
+    this.service.criar(this.pensamento).subscribe(() => {
+      this.router.navigate(['/listarPensamento'])
+    })
   }
 
   cancelar(){
-    alert("Ação cancelada")
+    this.router.navigate(['/listarPensamento'])
   }
 }
--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -16,5 +16,8 @@ export class PensamentoService {
     return this.http.get<Pensamento[]>(this.API)
   }
 
+  criar(pensamento: Pensamento): Observable<Pensamento> {
+   return this.http.post<Pensamento>(this.API, pensamento)
+  }
 
 }

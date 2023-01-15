87318d3b0e1a8c28cedb8bd481aa7ece286630b6
Lucas <lucasmeinen@yahoo.com.br>
2023-01-15 14:41:46 -0300

    Implementando funcionalidade excluirPensamento e cancelar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ExcluirPensamentoComponent } from './componentes/pensamentos/excluir-pensamento/excluir-pensamento.component';
 import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
 import { NgModule } from '@angular/core';
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     path: 'listarPensamento',
     component: ListarPensamentoComponent
+  },
+  {
+    path: 'pensamentos/excluirPensamento/:id',
+    component: ExcluirPensamentoComponent
   }
 ];
 
--- src/app/componentes/pensamentos/excluir-pensamento/excluir-pensamento.component.ts
+++ src/app/componentes/pensamentos/excluir-pensamento/excluir-pensamento.component.ts
@@ -1,4 +1,7 @@
+import { PensamentoService } from './../pensamento.service';
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Pensamento } from '../pensamento/pensamento';
 
 @Component({
   selector: 'app-excluir-pensamento',
@@ -6,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./excluir-pensamento.component.css']
 })
 export class ExcluirPensamentoComponent {
+  pensamento: Pensamento = {
+    id: 0,
+    conteudo: '',
+    autoria: '',
+    modelo: ''
+  }
+   constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private route: ActivatedRoute
+   ) {}
 
+   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.service.buscarPorId(parseInt(id!)).subscribe((pensamento) => {
+      this.pensamento = pensamento
+    })
+   }
+
+   excluirPensamento() {
+    if(this.pensamento.id){
+      this.service.excluir(this.pensamento.id).subscribe(() => {
+        this.router.navigate(['/listarPensamento'])
+      })
+    }
+  }
+
+  cancelar(){
+    this.router.navigate(['/listarPensamento'])
+  }
 }
